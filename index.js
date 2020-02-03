ebf721fa4310c81a9e727d1ebe8d46c113f802bd
Haseeb <haseeb.elahi@conradlabs.com>
2020-02-04 00:14:50 +0500

    Add error handler for broken image links and show default image

--- index.js
+++ index.js
@@ -262,7 +262,7 @@
             innerHTML += `
             <li>
                 <a title='${user.screen_name}' href='https://twitter.com/${user.screen_name}' target = '_blank'>
-                    <img class='avatar-img' src='${user.avatar_img}' alt='profile avatar'>
+                    <img class='avatar-img' src='${user.avatar_img}' alt='profile avatar' onerror="imageError(this)">
                     @${user.username}
                 </a> at ${user.tweet_time}
             </li>
